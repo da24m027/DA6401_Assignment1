26ef632a33869a7994e9347d258cdfa8f1e22505
da24m027 <da24m027@smail.iitm.ac.in>
2025-03-05 14:06:18 +0530

    Updated weights initialization in Neural Network

--- src/neuralnetwork.py
+++ src/neuralnetwork.py
@@ -1,11 +1,22 @@
 import numpy as np
 class NeuralNetwork:
-    def __init__(self, input_size, hidden_layers, hidden_size, output_size, activation="ReLU"):
+    def __init__(self, input_size, hidden_layers, hidden_size, output_size, activation="ReLU", init_method = "random"):
         self.layers = [input_size] + [hidden_size] * hidden_layers + [output_size]
-        self.weights = [np.random.randn(self.layers[i], self.layers[i+1]) * 0.01 for i in range(len(self.layers)-1)]
+        self.init_method = init_method
+        self.weights = self.initialize_weights()
         self.biases = [np.zeros((1, self.layers[i+1])) for i in range(len(self.layers)-1)]
         self.activation = activation
-
+    
+    def initialize_weights(self):
+        weights = []
+        for i in range(len(self.layers) - 1):
+            if self.init_method == "Xavier":
+                limit = np.sqrt(1 / self.layers[i])
+                weights.append(np.random.uniform(-limit, limit, (self.layers[i], self.layers[i+1])))
+            else:
+                weights.append(np.random.randn(self.layers[i], self.layers[i+1]) * 0.01)
+        return weights
+    
     def activation_func(self, x):
         if self.activation == "ReLU":
             return np.maximum(0, x)
